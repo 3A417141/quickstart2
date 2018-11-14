65e2c2318b2edcb2e37e883d3bf3ee3d164e72e9
3A417141 <as05202005@gmail.com>
2018-11-14 15:35:34 +0800

    修改TaskController，使其顯示 & 存放Task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 use App\Http\Requests;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class TaskController extends Controller
@@ -17,5 +17,33 @@ class TaskController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+    }
+	
+	/**
+     * 顯示使用者所有任務的清單。
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        return view('tasks.index');
+    }
+	
+	 /**
+     * 建立新的任務。
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+        return redirect('/tasks');
     }
 }
