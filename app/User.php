add2574dbb143fbfdfe034315cee5ad5561cbc8f
3A417141 <as05202005@gmail.com>
2018-11-14 15:17:17 +0800

    建立 Task model & 連結 Task 與 User model

--- app/User.php
+++ app/User.php
@@ -1,15 +1,13 @@
 <?php
-
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use Notifiable;
-
+	
     /**
      * The attributes that are mass assignable.
      *
@@ -18,7 +16,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password',
     ];
-
+	
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -27,4 +25,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+	
+    /**
+     * 取得該使用者的所有任務。
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
